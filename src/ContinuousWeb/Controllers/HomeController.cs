9c0c29129c5fdfce2c9c79601bc85bda5191fdcb
Chris Howd <cahowd@microsoft.com>
2017-02-26 14:40:09 -0800

    fixed

--- src/ContinuousWeb/Controllers/HomeController.cs
+++ src/ContinuousWeb/Controllers/HomeController.cs
@@ -15,10 +15,10 @@ namespace WebApplication.Controllers
 
         public IActionResult About()
         {
-            throw new NotImplementedException();
-            
-            // ViewData["Message"] = "Your application description page.";
-            //return View();
+            //throw new NotImplementedException();
+
+             ViewData["Message"] = "Your application description page.";
+            return View();
         }
 
         public IActionResult Contact()
