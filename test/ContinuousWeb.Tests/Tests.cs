6b861422572a0409929da6d7947e78de441e6b27
Chris Howd <cahowd@microsoft.com>
2017-02-26 14:02:47 -0800

    test added

---
+++ test/ContinuousWeb.Tests/Tests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using WebApplication.Controllers;
+using Xunit;
+
+namespace ContinuousWeb.Tests
+{
+    public class HomeControllerTests
+    {
+        [Fact]
+        public void AboutPageTest() 
+        {
+            var homeController = new HomeController();
+            var result = homeController.About();
+        }
+    }
+}
